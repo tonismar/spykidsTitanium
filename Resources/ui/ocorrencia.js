252a1b11e574ecaa58447889f7f11582db4e0368
tonismar <tonismar@gmail.com>
2014-05-15 00:04:32 -0300

    Marcação de uma ocorrência como visualizada.

--- Resources/ui/ocorrencia.js
+++ Resources/ui/ocorrencia.js
@@ -1,58 +1,77 @@
 function ocorrencia(occ) {
-var self = Ti.UI.createWindow({
-	orientation: 'vertical',
-	title: 'SpyKids'
-});
-var view = Ti.UI.createView();
+	var self = Ti.UI.createWindow({
+		orientation: 'vertical',
+		title: 'SpyKids'
+	});
+	var view = Ti.UI.createView();
+	
+	var avatar = Ti.UI.createImageView({
+		height : 150,
+		width : 150,
+		top : 4,
+		left : 80,
+		image : 'http://spykids.bl.ee/img/' + occ.getImagem()
+	});
+	
+	var user = Ti.UI.createLabel({
+		text : occ.getUser(),
+		top : 160,
+		center : true,
+		textAlign : 'center',
+		font : { fontWeight: 'bold' }
+	});
+	
+	var detail = Ti.UI.createLabel({
+		text: formatDate(occ.getDia()) + ' - ' + occ.getHora(),
+		top: 180,
+		font: { fontSize: 10 },
+		center: true,
+		textAlign: 'center' 
+	});
+	
+	var message = Ti.UI.createLabel({
+		text : occ.getMensagem(),
+		top : 170,
+		width: 140,
+		height: 140,
+		center: true,
+		textAlign: 'center',
+		wordWrap: true 
+	});
+	
+	var btn = Titanium.UI.createButton({
+		title: 'Visualizada',
+		textAlign: 'center',
+		top: 320
+	});
+	
+	view.add(avatar);
+	view.add(user);
+	view.add(detail);
+	view.add(message);
+	view.add(btn);
+	self.add(view); 
 
-var avatar = Ti.UI.createImageView({
-	height : 150,
-	width : 150,
-	top : 4,
-	left : 80,
-	image : 'http://spykids.bl.ee/img/' + occ.getImagem()
-});
+	btn.addEventListener('click', function() {
 
-var user = Ti.UI.createLabel({
-	text : occ.getUser(),
-	top : 160,
-	center : true,
-	textAlign : 'center',
-	font : { fontWeight: 'bold' }
-});
-
-var detail = Ti.UI.createLabel({
-	text: formatDate(occ.getDia()) + ' - ' + occ.getHora(),
-	top: 180,
-	font: { fontSize: 10 },
-	center: true,
-	textAlign: 'center' 
-});
-
-var message = Ti.UI.createLabel({
-	text : occ.getMensagem(),
-	top : 170,
-	width: 140,
-	height: 140,
-	center: true,
-	textAlign: 'center',
-	wordWrap: true 
-});
-
-var btn = Ti.UI.createButton({
-	title: 'Visualizada',
-	textAlign: 'center',
-	top: 320
-});
-
-view.add(avatar);
-view.add(user);
-view.add(detail);
-view.add(message);
-view.add(btn);
-self.add(view); 
-
-return self;
+		var update = Ti.Network.createHTTPClient();
+		update.onerror = function(e) {
+			alert('Erro ao marcar ocorrência como visualizada.');
+		};
+		
+		update.onload = function() {
+			var json = JSON.parse(this.responseText);
+			if (json.ocorrencias = 'Success') {
+				self.close();
+				alert('Ocorrência visualizada.');
+			};
+		};
+		
+		update.open('GET', 'http://spykids-tonismar.rhcloud.com/list.php?update=' + occ.getId());
+		update.send();
+	});	
+	
+	return self;
 }
 
 module.exports = ocorrencia;
